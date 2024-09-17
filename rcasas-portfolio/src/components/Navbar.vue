ec8d72603596e83a01b00d6d986e11a19b5b8cfa
Reymart <31478426+Rcasas14@users.noreply.github.com>
2024-09-17 12:12:24 +0800

    navbar in progress

---
+++ rcasas-portfolio/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="min-h-[76px] mx-[60px]">
+        <ul class="flex justify-start">
+            <li class="flex-none">Reymart Casas</li>
+            <li class="flex flex-row ">
+                <a href="#" class="navlinks">Home</a>
+                <a href="#" class="navlinks">Projects</a>
+            </li>
+            <li class="flex flex-row justify-self-end">
+                <a href="#" class="navlinks">Github</a>
+                <a href="" class="navlinks"><img src="../assets/dark-mode-icon.svg" alt=""></a>
+            </li>
+
+
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default{
+    data(){
+
+    }   
+}
+
+</script>
