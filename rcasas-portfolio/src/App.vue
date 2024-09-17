2aec34cc99965dba0db2419c3c7242b10346cf8b
Reymart <31478426+Rcasas14@users.noreply.github.com>
2024-09-17 12:12:12 +0800

    Initial Setup

---
+++ rcasas-portfolio/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <navbar></navbar>
+
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue';
+
+  export default{
+  components:{
+    Navbar,
+  },
+  data() {
+    return {
+      name: 'WORLD',
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
