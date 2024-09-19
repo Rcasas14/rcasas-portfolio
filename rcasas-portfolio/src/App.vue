cc86fae5c62c716bda498bfc024221d88a990886
romelbonnie <31478426+Rcasas14@users.noreply.github.com>
2024-09-19 17:03:51 +0800

    working on navigation

--- rcasas-portfolio/src/App.vue
+++ rcasas-portfolio/src/App.vue
@@ -5,16 +5,37 @@
 
 <script>
   import Navbar from './components/Navbar.vue';
+  import HeroSection from './components/HeroSection.vue';
 
   export default{
   components:{
     Navbar,
+    HeroSection,
   },
   data() {
     return {
-      name: 'WORLD',
+      navItems: [],
+      activeNavItem: 0,
+      darkNavBar: false,
     }
-  }
+  },
+  methods: {
+    async initNavItems(){
+      try {
+        let res = await fetch('./navItems.json');
+        let data = await res.json();
+
+        this.navItems = data;
+        console.log(this.navItems)
+        console.log(data)
+      }catch(err){
+        console.log('Something Error', err)
+      }
+    },
+  },
+  mounted(){
+    this.initNavItems();
+  },
 }
 </script>
 
--- rcasas-portfolio/src/components/Navbar.vue
+++ rcasas-portfolio/src/components/Navbar.vue
@@ -1,25 +1,27 @@
 <template>
-    <nav class="min-h-[76px] mx-[60px]">
-        <ul class="flex justify-start">
-            <li class="flex-none">Reymart Casas</li>
-            <li class="flex flex-row ">
-                <a href="#" class="navlinks">Home</a>
-                <a href="#" class="navlinks">Projects</a>
+    <div class="nav-wrapper border-b-[1px]">
+    <nav class="min-h-[76px] mx-[30px] ">
+        <ul class="flex justify-between items-center">
+            <li class=""><a href="#">Reymart Casas.</a></li>
+            <li class="flex-1 mx-10">
+                <a href="#" class="navlinks m-5">Home</a>
+                <a href="#" class="navlinks m-5">Projects</a>
             </li>
-            <li class="flex flex-row justify-self-end">
-                <a href="#" class="navlinks">Github</a>
-                <a href="" class="navlinks"><img src="../assets/dark-mode-icon.svg" alt=""></a>
+            <li class="flex items-center">
+                <a href="#" class="navlinks m-5">Github</a>
+                <a href="" class="navlinks m-5"><img src="../assets/dark-mode-icon.svg" alt=""></a>
             </li>
-
-
         </ul>
     </nav>
+</div>
 </template>
 
 <script>
 export default{
+    props: ['navItems', 'activeNavItem'],
     data(){
-
+        return {
+        }
     }   
 }
 
